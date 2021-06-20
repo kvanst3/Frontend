d8b1eccec6aa7186033899fd9b3b011cfbd0dc5f
kvanst <kvanst3@gmail.com>
2021-06-20 16:50:14 +0200

    dynamic content

--- Upgraded Blog/main.py
+++ Upgraded Blog/main.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import template_rendered
 from flask.templating import render_template
+import requests
 
 app = Flask(__name__)
 
+posts = requests.get("https://api.npoint.io/752bea529869fdd9037a").json()
+
 @app.route("/")
 def get_all_posts():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts)
+
+@app.route("/post/<int:id>")
+def post(id):
+    return render_template("post.html", post=posts[id -1])
 
 @app.route("/about")
 def about():
