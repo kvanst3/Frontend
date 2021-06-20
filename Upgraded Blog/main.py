9ba8308ebdea0c8bd2d658f541c6305856cc14ff
kvanst <kvanst3@gmail.com>
2021-06-20 17:52:03 +0200

    send email to owner

--- Upgraded Blog/main.py
+++ Upgraded Blog/main.py
@@ -1,6 +1,10 @@
 from flask import Flask, template_rendered, request
 from flask.templating import render_template
 import requests
+import smtplib
+import os
+
+OWN_EMAIL = os.environ.get("INDI_EMAIL")
 
 app = Flask(__name__)
 
@@ -22,12 +26,18 @@ def about():
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, os.environ.get("INDI_PW"))
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
