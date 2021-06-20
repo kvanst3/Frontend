8a9203a5af702195c30d2a4272f9620d06302be8
kvanst <kvanst3@gmail.com>
2021-06-20 14:05:07 +0200

    routes and links

--- Upgraded Blog/main.py
+++ Upgraded Blog/main.py
@@ -8,5 +8,13 @@ app = Flask(__name__)
 def get_all_posts():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def get_contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
