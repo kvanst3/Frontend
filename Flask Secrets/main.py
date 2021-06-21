27bc6872df58cbc53ffffc1ae6c9f938ef66d96e
kvanst <kvanst3@gmail.com>
2021-06-21 15:21:09 +0200

    swap to passwordfield

--- Flask Secrets/main.py
+++ Flask Secrets/main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 import os
 
 
 class MyForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
 
 
 app = Flask(__name__)
@@ -20,10 +20,10 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = MyForm()
-    # if request.method == "POST":
-    #     if form.validate_on_submit():
-    #         return redirect('success.html')
-    #     return render_template('denied.html')
+    if request.method == "POST":
+        if form.validate_on_submit():
+            return render_template('success.html')
+        return render_template('denied.html')
     return render_template("login.html", form=form)
 
 if __name__ == '__main__':
