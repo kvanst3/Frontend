c265c1ff52ff3bb1d5f2d8f5b4184f197d69a869
kvanst <kvanst3@gmail.com>
2021-06-21 15:46:52 +0200

    adding validators

--- Flask Secrets/main.py
+++ Flask Secrets/main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email
 import os
 
 
 class MyForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8), Email(message=None, granular_message=False, check_deliverability=False, allow_smtputf8=True, allow_empty_local=False)])
     submit = SubmitField(label="Log In")
 
 
