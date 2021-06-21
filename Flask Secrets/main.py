a07328fce4879ce795a725fabe6f0be0937c28e1
kvanst <kvanst3@gmail.com>
2021-06-21 15:38:57 +0200

    display errors after validation

--- Flask Secrets/main.py
+++ Flask Secrets/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 import os
 
@@ -8,6 +8,7 @@ import os
 class MyForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
@@ -23,7 +24,7 @@ def login():
     if request.method == "POST":
         if form.validate_on_submit():
             return render_template('success.html')
-        return render_template('denied.html')
+        return render_template('login.html', form=form)
     return render_template("login.html", form=form)
 
 if __name__ == '__main__':
