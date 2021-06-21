c74c0cc18a3a1fef3b4593cb5e7240465e30e940
kvanst <kvanst3@gmail.com>
2021-06-21 14:55:00 +0200

    route to login

--- Flask Secrets/main.py
+++ Flask Secrets/main.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+import os
+
+
+class MyForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+
 
 app = Flask(__name__)
-
+app.secret_key = os.environ.get("INDI_PW")
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
-
+    form = MyForm()
+    # if request.method == "POST":
+    #     if form.validate_on_submit():
+    #         return redirect('success.html')
+    #     return render_template('denied.html')
+    return render_template("login.html", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
